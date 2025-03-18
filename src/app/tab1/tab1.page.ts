4264ae7915b7abd3f594c563a1daf23742398339
ponta <190280@studierende.htl-donaustadt.at>
2025-03-18 14:21:18 +0100

    Initial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-tab1',
@@ -9,5 +10,8 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  constructor(http: HttpClient) {
+    http.get("tab1").subscribe();
+  }
 }
