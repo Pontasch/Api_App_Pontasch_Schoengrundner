25b2971546ffebd52ef953650381f16abefec4f4
ponta <190280@studierende.htl-donaustadt.at>
2025-03-31 10:58:39 +0200

    APP-16

--- src/app/auto/auto.page.ts
+++ src/app/auto/auto.page.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import {ExploreContainerComponent} from "../explore-container/explore-container.component";
+import {IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {Auto, FahrzeugeService} from "../api/fahrzeuge.service";
 
 @Component({
   selector: 'app-auto',
   templateUrl: './auto.page.html',
   styleUrls: ['./auto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ExploreContainerComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel]
 })
-export class AutoPage  {
+export class AutoPage  implements OnInit {
+  autos: Auto | undefined;
 
-  constructor() { }
-
-
-
+  constructor(private fahrzeugeService: FahrzeugeService) {}
+  ngOnInit(): void {
+    this.fahrzeugeService.getAutosWithAttributes(2020).subscribe(
+      (data) => {
+        this.autos = data;
+        console.log('Autos:', this.autos);
+      },
+      (error) => {
+        console.error('Fehler beim Abrufen der Autos:', error);
+      }
+    );
+  }
 }
