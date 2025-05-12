3335c7187e16583850796eeb6b0a6cba85428b32
inesschoen <210179@studierende.htl-donaustadt.at>
2025-05-12 08:50:30 +0200

    Directory Aenderung

--- src/app/motorrad/motorrad.page.ts
+++ src/app/motorrad/motorrad.page.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import {ExploreContainerComponent} from "../explore-container/explore-container.component";
+import {FahrzeugeService, Motorraeder} from "../api/fahrzeuge.service";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-motorrad',
   templateUrl: './motorrad.page.html',
   styleUrls: ['./motorrad.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ExploreContainerComponent]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
-export class MotorradPage  {
+export class MotorradPage implements OnInit {
 
-  constructor() { }
+  motorraeder: Motorraeder | undefined;
+  Marke: string = "";
+  Model: string = "";
+  selectedYear: number = 2020;
+  availableYears: number[] = [
+    2020, 2019, 2018, 2017, 2016, 2015
+  ];
 
+  constructor(
+    private fahrzeugeService: FahrzeugeService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loadMotorraeder();
+  }
+
+  loadMotorraeder() {
+    this.fahrzeugeService.getMotorraeder(this.Marke).subscribe(
+      (data) => {
+        this.motorraeder = data;
+      },
+      (error) => {
+        console.error('Fehler:', error);
+      }
+    );
+  }
 }
